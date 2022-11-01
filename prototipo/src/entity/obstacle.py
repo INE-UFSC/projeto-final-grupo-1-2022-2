ccc11d149694bb49c382553e87a91149706210a0
TheHungaro <uzomabenedek@gmail.com>
2022-11-01 11:36:22 -0300

    add obstacle

--- prototipo/src/entity/obstacle.py
+++ prototipo/src/entity/obstacle.py
@@ -1,8 +1,18 @@
 from .entity import Entity
+from ..components import RenderComponent, MoveComponent, CollisionComponent
+from ..library import CubeCollider
 
 
 class Obstacle(Entity):
-    ...
+    def __init__(self, pos, size, velocity = (0, 0, 0), color = "#ffff00"):
+        render = RenderComponent((size[0], size[1] + size[2]), color)
+        move = MoveComponent(pos, velocity, True)
+
+        collider = CubeCollider(move.pos, size)
+        collision = CollisionComponent(collider)
+
+        super().__init__(render, move, collision)
+
 
 
 class Student(Obstacle):
