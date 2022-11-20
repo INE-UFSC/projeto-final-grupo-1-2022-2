235b4c9c699edf20f4487c3a73dbd6fa565c9fda
enzoBrum <109223503+enzoBrum@users.noreply.github.com>
2022-11-20 15:22:32 -0300

    Remove unused method from Menu

--- versao_final/src/menu/menu.py
+++ versao_final/src/menu/menu.py
@@ -14,18 +14,7 @@ class Menu:
     def __init__(self, components):
         self.__components = components
 
-    def update(self):
-        # atualiza todos os componentes do menu
-        # com base nas teclas pressionadas e na
-        # posição do mouse
-
-        keyboard_keys = pg.key.get_focused()
-        mouse_keys = pg.mouse.get_pressed()
-        keys = All_keys(keyboard_keys, mouse_keys)
-
-        mouse_pos = pg.Vector2(pg.mouse.get_pos())
-        for component in self.__components:
-            component.update(keys, mouse_pos)
+    
 
     def render(self, screen: pg.Surface):
         # renderiza todos os componentes do menu
