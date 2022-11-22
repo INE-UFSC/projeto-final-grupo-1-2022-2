c889f411f8f08e50605b6c0a9676aff6a68bccdc
darosabrumenzo@gmail.com <darosabrumenzo@gmail.com>
2022-11-21 22:57:11 -0300

    add event logic and grid layout

--- versao_final/src/menu/menu.py
+++ versao_final/src/menu/menu.py
@@ -1,22 +1,68 @@
+from abc import ABC
 from typing import List
 
 import pygame as pg
 
+from ..icontrol import IControl
+from ..library import Listener
 from .components import MenuComponent
 
 
-class Menu:
-    __components: List[MenuComponent]
+# TODO
+# função para instanciar o menu no centro da tela
+class Menu(ABC, Listener):
 
-    def __init__(self, components):
+    __components: List[
+        List[MenuComponent]
+    ]  # cada elemento corresponde à uma linha do menu
+    __control: IControl
+    __h_padding: int = 50
+    __v_padding: int = 50
+
+    __h_spacing: int = 50
+    __v_spacing: int = 100
+
+    def __init__(self, components: List[List[MenuComponent]], control: IControl):
+        super().__init__()
         self.__components = components
+        self.__control = control
+        for line in self.__components:
+            for component in line:
+                self.subscribe(component)
+                component.event = self.control.event
 
-    def render(self, screen: pg.Surface):
+    def render(self):
         # renderiza todos os componentes do menu
+        for line in self.__components:
+            for component in line:
+                component.render(self.control.screen.display)
 
-        for component in self.__components:
-            component.render(screen)
+    def create_grid_layout(
+        self,
+        components: List[List[MenuComponent]],
+    ) -> List[List[MenuComponent]]:
+        """
+        cria um grid com os `components` por meio da
+        alteração da posição destes
+        """
+        screen_size = pg.Vector2(self.control.screen.size)
+        for i, line in enumerate(components):
+            y_pos = self.__v_padding + i * self.__v_spacing
+
+            for j, component in enumerate(line):
+                size = component.size
+                x_pos = self.__h_padding + j * self.__h_spacing
+                component.pos = (x_pos, y_pos)
+        return components
 
     @property
     def components(self):
         return self.__components
+
+    @property
+    def control(self):
+        return self.__control
+
+    @components.setter
+    def components(self, components: List[List[MenuComponent]]):
+        self.__components = components
