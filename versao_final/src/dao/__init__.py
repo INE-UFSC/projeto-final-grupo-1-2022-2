6554a2177e8382dc9fb2f05338cbf2ad35c29370
TheHungaro <uzomabenedek@gmail.com>
2022-11-25 11:06:43 -0300

    Add leaderboard json and DAO

--- versao_final/src/dao/__init__.py
+++ versao_final/src/dao/__init__.py
@@ -1,9 +1,11 @@
 from .resourceDAO import ResourceDAO
 from .textureDAO import TextureDAO
 from .mapDAO import MapDAO
+from .leaderboardDAO import LeaderboardDAO
 
 __all__ = [
     "ResourceDAO",
     "TextureDAO",
     "MapDAO",
+    "LeaderboardDAO",
 ]
---
+++ versao_final/src/dao/leaderboardDAO.py
@@ -0,0 +1,47 @@
+
+import json
+import time
+from pathlib import Path
+from typing import Dict
+
+import pygame as pg
+
+from .resourceDAO import ResourceDAO
+
+
+class LeaderboardDAO(ResourceDAO):
+    _cache: Dict[str, pg.Surface]
+    _path = "leaderboard.json"
+
+    def load(self):
+        path = self.get_path(self._path)
+
+        if path not in self._cache:
+            try:
+                leaderboard = json.load(open(path, "rt", encoding="utf-8"))
+                print(leaderboard)
+            except FileNotFoundError:
+                return
+            except json.decoder.JSONDecodeError:
+                # use logger
+                print(f"[ERROR] invalid json format for leaderboard in file '{self._path}'.")
+                return
+            
+            self._cache = leaderboard
+
+        return self._cache.get(path)
+
+    def save(self, name, difficulty, score):
+        path = Path(self.get_path(self._path))
+        path.parent.mkdir(parents=True, exist_ok=True)
+
+        players = self._cache.setdefault("players", {})
+        player = players.setdefault(name, {})
+        difficulty_obj = player.setdefault(difficulty, {})
+        
+        difficulty_obj["highscore"] = score
+        difficulty_obj["timestamp"] = time.time()
+        
+        txt = json.dumps(self._cache)
+
+        path.write_text(txt, encoding="utf-8")
