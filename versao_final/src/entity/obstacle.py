2de42ce2310267422bb2c86209b74edc32677eb3
Joao Pedro Schmidt Cordeiro <jschmidtcordeiro@gmail.com>
2022-12-06 17:28:42 -0700

    Remove sprites background
    Signed-off-by: Joao Pedro Schmidt Cordeiro <jschmidtcordeiro@gmail.com>

--- versao_final/src/entity/obstacle.py
+++ versao_final/src/entity/obstacle.py
@@ -1,3 +1,5 @@
+import pygame as pg
+
 from ..components import CollisionComponent, PosComponent, MoveComponent, RenderComponent
 from ..library import CubeCollider, class_name
 from ..dao import TextureDAO
@@ -5,17 +7,27 @@ from .entity import Entity
 
 
 class Obstacle(Entity):
-    def __init__(self, pos, size, velocity=None, color="#ffff00", climb_height: float = -1):
+    def __init__(
+        self,
+        pos,
+        size,
+        velocity=None,
+        color="#ffff00",
+        climb_height: float = -1,
+        surface: pg.Surface = None,
+        alpha: bool = False
+    ):
         pos_comp = PosComponent(pos)
         move = MoveComponent(velocity) if velocity else None
 
         collider = CubeCollider(pos_comp.value, size)
         collision = CollisionComponent(collider, climb_height)
 
-        texture_path = f"{class_name(self)}.png"
-        surface = TextureDAO().load(texture_path, lambda: collider.get_surface(color))
+        if surface is None:
+            texture_path = f"{class_name(self)}.png"
+            surface = TextureDAO().load(texture_path, lambda: collider.get_surface(color))
 
-        render = RenderComponent(surface)
+        render = RenderComponent(surface, alpha=alpha)
 
         super().__init__(pos_comp, move, collision, render)
 
@@ -26,7 +38,7 @@ class Handrail(Obstacle):
     """
     def __init__(self, pos, size=[10, 50, 120], color="#F5BB00"):
         self.__pos_dislocated = (pos[0]+ 40, pos[1], pos[2])
-        super().__init__(self.__pos_dislocated, size, color=color)
+        super().__init__(self.__pos_dislocated, size, color=color, alpha=True)
 
 
 class PartyAdsTable(Obstacle):
@@ -34,7 +46,7 @@ class PartyAdsTable(Obstacle):
     Obstacle that represents a party advertisement table
     """
     def __init__(self, pos, size=[100, 50, 50], color="#ffffff"):
-        super().__init__(pos, size, color=color)
+        super().__init__(pos, size, color=color, alpha=True)
 
 
 class SmallBush(Obstacle):
@@ -42,7 +54,7 @@ class SmallBush(Obstacle):
     Obstacle that represents a small bush
     """
     def __init__(self, pos, size=[50, 50, 10], color="#60f731"):
-        super().__init__(pos, size, color=color)
+        super().__init__(pos, size, color=color, alpha=True)
 
 
 class BigBush(Obstacle):
@@ -50,7 +62,7 @@ class BigBush(Obstacle):
     Obstacle that represents a big bush
     """
     def __init__(self, pos, size=[120, 80, 50], color="#09ad19"):
-        super().__init__(pos, size, color=color)
+        super().__init__(pos, size, color=color, alpha=True)
 
 
 class Bike(Obstacle):
@@ -58,7 +70,7 @@ class Bike(Obstacle):
     Obstacle that represents a bike
     """
     def __init__(self, pos, size=[20, 50, 60], velocity=(0, 0, 0), color="#003B36"):
-        super().__init__(pos, size, velocity, color)
+        super().__init__(pos, size, velocity, color, alpha=True)
 
 
 class Student(Obstacle):
@@ -66,7 +78,7 @@ class Student(Obstacle):
     Obstacle that represents a student
     """
     def __init__(self, pos, size=[60, 110, 4], color="#ffc294"):
-        super().__init__(pos, size, color=color)
+        super().__init__(pos, size, color=color, alpha=True)
 
 
 class Car(Obstacle):
@@ -75,7 +87,7 @@ class Car(Obstacle):
     """
     def __init__(self, pos, size=[100, 60, 110], velocity=(0, 0, 0), color="#222222"):
         self.__climb_height = 20
-        super().__init__(pos, size, velocity, color, self.__climb_height)
+        super().__init__(pos, size, velocity, color, self.__climb_height, alpha=True)
 
 
 class Bus(Obstacle):
