512a50471e9efe4019ba3a0655e61868a20d8881
William Kraus <88288206+KrLiam@users.noreply.github.com>
2022-12-09 23:34:57 -0300

    make config attributes private

--- versao_final/src/config.py
+++ versao_final/src/config.py
@@ -5,35 +5,47 @@ from .library import Singleton
 
 
 class Config(metaclass=Singleton):
-    
-    
-    screen_size: Tuple[int, int] = (1280, 760)
+    __screen_size: Tuple[int, int] = (1280, 760)
 
     __difficulty: str = "easy"
     __default_player_name: str = "Graduando Desconhecido"
-    
-    lane_amount: int = 3
-    lane_width: int = 120
+    __lane_amount: int = 3
+    __lane_width: int = 120
 
-    framerate: int = 60
-    max_deltatime: float = 0.1
+    __framerate: int = 60
+    __max_deltatime: float = 0.1
 
-    destruction_distance: int = 1000
+    __destruction_distance: int = 1000
 
-    slide_duration: float = 0.25
-    gravity: float = -1600
-    jump_force: float = 580
-    crouch_duration: int = 1000  # crouch duration in milliseconds
+    __slide_duration: float = 0.25
+    __gravity: float = -1600
+    __jump_force: float = 580
+    __crouch_duration: int = 1000  # crouch duration in milliseconds
 
-    camera_y_offset: int = -100
+    __camera_y_offset: int = -100
 
-    def __init__(self): self.__parse_args()
+    def __init__(self):
+        self.__parse_args()
+
     def __parse_args(self):
         parser = ArgumentParser()
-        parser.add_argument("--save-textures", default=False, action="store_true", help="Save loaded and generated textures.")
+        parser.add_argument(
+            "--save-textures",
+            default=False,
+            action="store_true",
+            help="Save loaded and generated textures.",
+        )
         self.__args = parser.parse_args()
+
+
     @property
-    def args(self): return self.__args
+    def args(self):
+        return self.__args
+
+    @property
+    def screen_size(self):
+        return self.__screen_size
+
     @property
     def difficulty(self):
         return self.__difficulty
@@ -42,10 +54,47 @@ class Config(metaclass=Singleton):
     def difficulty(self, value):
         self.__difficulty = value
 
-
     @property
     def default_player_name(self):
         return self.__default_player_name
 
-    
 
+    @property
+    def lane_amount(self):
+        return self.__lane_amount
+
+    @property
+    def lane_width(self):
+        return self.__lane_width
+
+    @property
+    def framerate(self):
+        return self.__framerate
+
+    @property
+    def max_deltatime(self):
+        return self.__max_deltatime
+
+    @property
+    def destruction_distance(self):
+        return self.__destruction_distance
+
+    @property
+    def slide_duration(self):
+        return self.__slide_duration
+
+    @property
+    def gravity(self):
+        return self.__gravity
+
+    @property
+    def jump_force(self):
+        return self.__jump_force
+
+    @property
+    def crouch_duration(self):
+        return self.__crouch_duration
+
+    @property
+    def camera_y_offset(self):
+        return self.__camera_y_offset
